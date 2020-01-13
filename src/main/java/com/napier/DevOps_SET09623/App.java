43b679a9d4d7374c75afe2e1cb5e68908dbca9b1
Maung Maung Oo <40437404@live.napier.ac.uk>
2020-01-14 00:04:43 +0630

    get top populated cities

--- src/main/java/com/napier/DevOps_SET09623/App.java
+++ src/main/java/com/napier/DevOps_SET09623/App.java
@@ -6,6 +6,30 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        App app = new App();
+
+        // Connect to database
+        app.connect();
+        // Get Employee
+        City cty = app.topPopulatedCities(10);
+        // Display results
+        app.displayTopPopulatedCities(cty);
+
+        // Disconnect from database
+        app.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
@@ -17,9 +41,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +52,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "root123!@#");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +64,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -57,6 +82,51 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+    public City topPopulatedCities(int limit)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT * FROM city order by Population DESC LIMIT " + limit;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                City cty = new City();
+                cty.id = rset.getInt("ID");
+                cty.name = rset.getString("Name");
+                cty.countrycode = rset.getString("CountryCode");
+                cty.district = rset.getString("District");
+                cty.population = rset.getInt("Population");
+                return cty;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get top populated cities");
+            return null;
+        }
+    }
+    public void displayTopPopulatedCities(City cty)
+    {
+        if (cty != null)
+        {
+            System.out.println(
+                    "ID: " + cty.id + "\n" +
+                            "Name: " + cty.name + "\n" +
+                            "Country Code: " + cty.countrycode + "\n" +
+                            "District: " + cty.district + "\n" +
+                            "Population: " + cty.population
+            );
         }
     }
 }
---
+++ src/main/java/com/napier/DevOps_SET09623/City.java
@@ -0,0 +1,32 @@
+package com.napier.DevOps_SET09623;
+
+/**
+ * Represents a city
+ */
+public class City {
+
+    /**
+     * City ID
+     */
+    public int id;
+
+    /**
+     * City name
+     */
+    public String name;
+
+    /**
+     * Country code of the country of that city
+     */
+    public String countrycode;
+
+    /**
+     * District name
+     */
+    public String district;
+
+    /**
+     * Population of the city
+     */
+    public int population;
+}
