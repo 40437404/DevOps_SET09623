582dd22fdb35ea5984001c7e4d7c97c22c72bc10
Maung Maung Oo <40437404@live.napier.ac.uk>
2020-01-16 15:58:15 +0630

    Get population of Continent

--- src/main/java/com/napier/DevOps_SET09623/App.java
+++ src/main/java/com/napier/DevOps_SET09623/App.java
@@ -12,12 +12,11 @@ public class App
         // Connect to database
         app.connect();
         // Number of cities
-        String region = "Caribbean";
+        String continent = "Asia";
         // Get Population
-        int[] result = new int[3];
-        result = app.getPopulationOfRegion(region);
+        long[] result = app.getPopulationOfContinent(continent);
         // Display results
-        app.displayPopulationOfCountry(region, result);
+        app.displayPopulationOfContinent(continent, result);
 
         // Disconnect from database
         app.disconnect();
@@ -87,7 +86,7 @@ public class App
             }
         }
     }
-    public int[] getPopulationOfRegion(String region)
+    public long[] getPopulationOfContinent(String continent)
     {
         try
         {
@@ -95,41 +94,36 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String getRegionPopulation = String.format(
-                    "SELECT Population FROM country WHERE Region='%s';"
-                    , region);
+                    "SELECT SUM(Population) FROM country WHERE Continent='%s';"
+                    , continent);
             // Execute SQL statement
             ResultSet rset1 = stmt.executeQuery(getRegionPopulation);
             // Return population if valid.
-            int population = 0;
-            if (rset1.next() == false)
+            long population;
+            if (!rset1.next())
             {
                 return null;
             }
             else {
-                do
-                {
-                    population += rset1.getInt("Population");
-                } while(rset1.next());
+                population = rset1.getLong("SUM(Population)");
             }
-            String getCityPopulationOfRegion = String.format(
-                    "SELECT city.Population FROM city INNER JOIN country WHERE " +
-                            "city.CountryCode=country.Code AND country.Region='%s';"
-                    , region);
+            String getCityPopulationOfContinent = String.format(
+                    "SELECT SUM(city.Population) FROM city INNER JOIN country WHERE " +
+                            "city.CountryCode=country.Code AND country.Continent='%s';"
+                    , continent);
             // Execute SQL statement
-            ResultSet rset2 = stmt.executeQuery(getCityPopulationOfRegion);
+            ResultSet rset2 = stmt.executeQuery(getCityPopulationOfContinent);
             // Return population if valid.
-            int populationOfCity = 0;
-            if (rset2.next() == false)
+            long populationOfCity = 0;
+            if (!rset2.next())
             {
                 return null;
             }
             else {
-                do {
-                    populationOfCity += rset2.getInt("Population");
-                } while (rset2.next());
+                populationOfCity += rset2.getLong("SUM(city.Population)");
             }
-            int populationOfNotCity = population - populationOfCity;
-            int[] result = new int[3];
+            long populationOfNotCity = population - populationOfCity;
+            long[] result = new long[3];
             result[0] = population;
             result[1] = populationOfCity;
             result[2] = populationOfNotCity;
@@ -142,13 +136,13 @@ public class App
             return null;
         }
     }
-    public  void displayPopulationOfCountry(String country, int[] result)
+    public  void displayPopulationOfContinent(String continent, long[] result)
     {
         if (result != null)
         {
             System.out.println(
-                    "Region Name: " + country + "\n" +
-                            "Population of Region: " + result[0] + "\n" +
+                    "Continent Name: " + continent + "\n" +
+                            "Population of Continent: " + result[0] + "\n" +
                             "Population in Cities: " + result[1] + "\n" +
                             "Population outside Cities: " + result[2]
             );
