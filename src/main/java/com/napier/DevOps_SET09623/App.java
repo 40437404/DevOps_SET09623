71c72d1688a023b826f226ffe9d1784cb7bcf307
Maung Maung Oo <40437404@live.napier.ac.uk>
2020-01-16 14:10:54 +0630

    Get population of Country

--- src/main/java/com/napier/DevOps_SET09623/App.java
+++ src/main/java/com/napier/DevOps_SET09623/App.java
@@ -6,6 +6,33 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        App app = new App();
+
+        // Connect to database
+        app.connect();
+        // Number of cities
+        String country = "Japan";
+        // Get Population
+        int[] result = new int[3];
+        result = app.getPopulationOfCountry(country);
+        // Display results
+        app.displayPopulationOfCountry(country, result);
+
+        // Disconnect from database
+        app.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
@@ -17,9 +44,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +55,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "root123!@#");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +67,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -57,6 +85,70 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+    public int[] getPopulationOfCountry(String country)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String getCountryPopulation = String.format(
+                    "SELECT Population FROM country WHERE Name='%s';"
+                    , country);
+            // Execute SQL statement
+            ResultSet rset1 = stmt.executeQuery(getCountryPopulation);
+            // Return population if valid.
+            int population;
+            if (rset1.next() == false)
+            {
+                return null;
+            }
+            else {
+                population = rset1.getInt("Population");
+            }
+            String getCityPopulationOfCountry = String.format(
+                    "SELECT city.Population FROM city INNER JOIN country WHERE " +
+                            "city.CountryCode=country.Code AND country.Name='%s';"
+                    , country);
+            // Execute SQL statement
+            ResultSet rset2 = stmt.executeQuery(getCityPopulationOfCountry);
+            // Return population if valid.
+            int populationOfCity = 0;
+            if (rset2.next() == false)
+            {
+                return null;
+            }
+            else {
+                do {
+                    populationOfCity += rset2.getInt("Population");
+                } while (rset2.next());
+            }
+            int populationOfNotCity = population - populationOfCity;
+            int[] result = new int[3];
+            result[0] = population;
+            result[1] = populationOfCity;
+            result[2] = populationOfNotCity;
+            return result;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get population");
+            return null;
+        }
+    }
+    public  void displayPopulationOfCountry(String country, int[] result)
+    {
+        if (result != null)
+        {
+            System.out.println(
+                    "Country Name: " + country + "\n" +
+                            "Population of Country: " + result[0] + "\n" +
+                            "Population in Cities: " + result[1] + "\n" +
+                            "Population outside Cities: " + result[2]
+            );
         }
     }
 }
